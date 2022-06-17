c4ec8676e6e1a2bc233202dda83baa1fd0a270af
kilb <git@bll.io>
2022-06-17 21:46:52 +0800

    update

--- scripts/testHoneypot.js
+++ scripts/testHoneypot.js
@@ -1,28 +1,45 @@
 const ethers = require("ethers");
 const clipABI = require("../contracts/clip.json");
+const pairs = require("./transfer.json");
+// const pancakeABI = require("../contracts/pancake.json");
 
 const clipInterface = new ethers.utils.Interface(clipABI);
 const fromAddress = "0xA2cA1241A01B2fE1A9B56765aC66C1a13F131314";
 const clipAddress = "0x6A11823BfA6eda019512bAEEa7Fb25A6dE179715";
+const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545");
+// const poolContract = new ethers.Contract("0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73", pancakeABI, provider);
 
-const pairAddress = "0x9a57a0B448bFad9B1858b2D5248907bB4bA0B213"
-const data1 = clipInterface.encodeFunctionData("testHoneypot", [
-    pairAddress,
-    0,
-    ethers.utils.parseEther("1"),
-]);
+// const pairAddress = "0x9a57a0B448bFad9B1858b2D5248907bB4bA0B213";
+
+
+const stableCoins = ["BUSD", "USDT", "USDC", "DAI", "TUSD"];
 
 (async () => {
-  const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545");
-  for(i = 0; i < 100; ++i) {
-  const response = await provider.send("debug_traceCall", [
-    {
-      "from": fromAddress,
-      "to": clipAddress,
-      "data": data1,
-    },
-    "latest",
-  ]);
-//   console.log(response);
-  }
+  var idx = 0;
+  const len = pairs.length;
+  pairs.forEach(async (pair) => {
+      const pairAddress = pair["pair"];
+      var outId = 0;
+      if (pair["token0Symbol"] in stableCoins) {
+          outId = 1;
+      }
+      const data = clipInterface.encodeFunctionData("testHoneypot", [
+        pairAddress,
+        0,
+        ethers.utils.parseEther("1"),
+    ]);
+    const response = await provider.send("debug_traceCall", [
+        {
+            "from": fromAddress,
+            "to": clipAddress,
+            "data": data,
+        },
+        "latest",
+    ]);
+    if(response.failed) {
+        console.log(idx,"/" , len, " ", pairAddress);
+    }
+    idx += 1;
+
+  });
 })();
