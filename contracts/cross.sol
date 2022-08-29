25aa2d758bfcc12381403798f8dfc8bfff8d5337
kilb <git@bll.io>
2022-08-30 00:55:58 +0800

    fix bug

--- contracts/cross.sol
+++ contracts/cross.sol
@@ -38,30 +38,30 @@ contract CROSS2022V1 {
     }
 
     modifier onlyTrader {
-        require(tx.origin == 0x6994Cb5F2baF25BFE8Ca2E49fD1Cec5D8559a16c
-                || msg.sender == 0x46e3702Fe8a5c5532e368D768418b3cacF1623eE
-                || msg.sender == 0x0c9Fc86153c0219BD9EA432A05A20F280a3a7c8f
-                || msg.sender == 0x0CA7C62D2b0abF4B64f04686d0E7cF52Da9a9D11
-                || msg.sender == 0x859d2D5Cf3E02C667702B9098C389dB26559A671
-                || msg.sender == 0xEaAeadA6F22e4EA5ed9710C111d322566125433B
-                || msg.sender == 0xCb0b64205c3A03a6D19895862f00706d16f11fF4
-                || msg.sender == 0x78385cbCF1c3143Eb206f5Dd084D30697d85b9b7
-                || msg.sender == 0x43f8FE4F62C9bD35665baB792bb7f8e1A8546f3d
-                || msg.sender == 0xCf11DC3d0731c45D57395289e187143f7C30c793
-                || msg.sender == 0xE1DbBD96156FC5C7D320F40c7726356EC47c22D2
-                || msg.sender == 0xB42A80B14738e24A24B0F321Db1A07e3094e60a5
-                || msg.sender == 0xfA67FC194B8B2B34dFB227602eAbcE9123D3B1b3
-                || msg.sender == 0xfC4B1fd3751169c979d8954F468407e271C8dCD8
-                || msg.sender == 0x2389Df867bE7C495b25c0BA82e419d330bbff588
-                || msg.sender == 0x12DCC6f82847Bb84786EABDd26c7fd07C4121e6e
-                || msg.sender == 0xa95A28C7d4fDE72Db2fE42830860301dEC98E3C4
-                || msg.sender == 0xB3C98Ba64E0253f2B52aa96b4c2185f2E6Fdfb81
-                || msg.sender == 0x7db8Fe6a847BaC07ece602e40Cf21915BfB6ce60
-                || msg.sender == 0x5c0Cd9fc51808FAb0D83a480885067830913F2Fb
-                || msg.sender == 0xbe93E335B694c6Fdcb39167E5c67335f7b80039e
-                || msg.sender == 0xa79131662ADcCA5b09aB927eF690a2C4deE23dC5
-                || msg.sender == 0x6c3B7F6F177fFb38c06685A393f5f9128ECC0e99
-                || msg.sender == 0x3991993314484365851296601167350203279711);
+        require(tx.origin == 0x6c3B7F6F177fFb38c06685A393f5f9128ECC0e99
+                || tx.origin == 0x46e3702Fe8a5c5532e368D768418b3cacF1623eE
+                || tx.origin == 0x0c9Fc86153c0219BD9EA432A05A20F280a3a7c8f
+                || tx.origin == 0x0CA7C62D2b0abF4B64f04686d0E7cF52Da9a9D11
+                || tx.origin == 0x859d2D5Cf3E02C667702B9098C389dB26559A671
+                || tx.origin == 0xEaAeadA6F22e4EA5ed9710C111d322566125433B
+                || tx.origin == 0xCb0b64205c3A03a6D19895862f00706d16f11fF4
+                || tx.origin == 0x78385cbCF1c3143Eb206f5Dd084D30697d85b9b7
+                || tx.origin == 0x43f8FE4F62C9bD35665baB792bb7f8e1A8546f3d
+                || tx.origin == 0xCf11DC3d0731c45D57395289e187143f7C30c793
+                || tx.origin == 0xE1DbBD96156FC5C7D320F40c7726356EC47c22D2
+                || tx.origin == 0xB42A80B14738e24A24B0F321Db1A07e3094e60a5
+                || tx.origin == 0xfA67FC194B8B2B34dFB227602eAbcE9123D3B1b3
+                || tx.origin == 0xfC4B1fd3751169c979d8954F468407e271C8dCD8
+                || tx.origin == 0x2389Df867bE7C495b25c0BA82e419d330bbff588
+                || tx.origin == 0x12DCC6f82847Bb84786EABDd26c7fd07C4121e6e
+                || tx.origin == 0xa95A28C7d4fDE72Db2fE42830860301dEC98E3C4
+                || tx.origin == 0xB3C98Ba64E0253f2B52aa96b4c2185f2E6Fdfb81
+                || tx.origin == 0x7db8Fe6a847BaC07ece602e40Cf21915BfB6ce60
+                || tx.origin == 0x5c0Cd9fc51808FAb0D83a480885067830913F2Fb
+                || tx.origin == 0xbe93E335B694c6Fdcb39167E5c67335f7b80039e
+                || tx.origin == 0xa79131662ADcCA5b09aB927eF690a2C4deE23dC5
+                || tx.origin == 0x6994Cb5F2baF25BFE8Ca2E49fD1Cec5D8559a16c
+                || tx.origin == 0x3991993314484365851296601167350203279711);
         _;
     }
 
@@ -224,7 +224,6 @@ contract CROSS2022V1 {
         unchecked {
             // 检查是否已经被抢
             uint256 pairInfo;
-            IERC20 tokenIn;
             uint256 reserveIn;
             uint256 reserveOut;
             
@@ -253,10 +252,8 @@ contract CROSS2022V1 {
             pair = address(uint160(pairInfo));
             outId = (pairInfo >> 160) & 0xf;
             if (outId == 0) {
-                tokenIn = IERC20(IPancakePair(pair).token1());
                 (reserveOut, reserveIn, ) = IPancakePair(pair).getReserves();
             } else {
-                tokenIn = IERC20(IPancakePair(pair).token0());
                 (reserveIn, reserveOut, ) = IPancakePair(pair).getReserves();
             }
 
@@ -264,8 +261,6 @@ contract CROSS2022V1 {
             assembly {
                 mstore(add(pairInfos, 32), amountIn)
             }
-
-            uint256 balance0 = tokenIn.balanceOf(address(this));
             
             amountIn *= (pairInfo >> 176);
             uint256 amountOut = (amountIn * reserveOut) / (reserveIn * 10000 + amountIn);
@@ -274,8 +269,6 @@ contract CROSS2022V1 {
             } else {
                 IPancakePair(pair).swap(0, amountOut, address(this), pairInfos);
             }
-
-            require(tokenIn.balanceOf(address(this)) > balance0, "E001");
         }
     }
 
