e7690d7835e544b138f90db7e83dfca1343d5985
kilb <git@bll.io>
2022-11-03 14:22:16 +0800

    update

--- contracts/cross.sol
+++ contracts/cross.sol
@@ -14,27 +14,6 @@ contract CROSS2022V1 {
         _admins[msg.sender] = 1;
         _withdrawals[msg.sender] = 1;
         _buyerBank = ITokenBank(buyerBank);
-        _coinbases[0] = 0x2465176C461AfB316ebc773C61fAEe85A6515DAA;
-        _coinbases[1] = 0x295e26495CEF6F69dFA69911d9D8e4F3bBadB89B;
-        _coinbases[2] = 0x2b3A6c089311b478Bf629C29D790A7A6db3fc1b9;
-        _coinbases[3] = 0x2D4C407BBe49438ED859fe965b140dcF1aaB71a9;
-        _coinbases[4] = 0x3f349bBaFEc1551819B8be1EfEA2fC46cA749aA1;
-        _coinbases[5] = 0x61Dd481A114A2E761c554B641742C973867899D3; // change --
-        _coinbases[6] = 0x685B1ded8013785d6623CC18D214320b6Bb64759;
-        _coinbases[7] = 0x70F657164e5b75689b64B7fd1fA275F334f28e18;
-        _coinbases[8] = 0x72b61c6014342d914470eC7aC2975bE345796c2b;
-        _coinbases[9] = 0x7AE2F5B9e386cd1B50A4550696D957cB4900f03a;
-        _coinbases[10] = 0x8b6C8fd93d6F4CeA42Bbb345DBc6F0DFdb5bEc73;
-        _coinbases[11] = 0x9F8cCdaFCc39F3c7D6EBf637c9151673CBc36b88;
-        _coinbases[12] = 0xa6f79B60359f141df90A0C745125B131cAAfFD12;
-        _coinbases[13] = 0xAAcF6a8119F7e11623b5A43DA638e91F669A130f;
-        _coinbases[14] = 0xac0E15a038eedfc68ba3C35c73feD5bE4A07afB5;
-        _coinbases[15] = 0xBe807Dddb074639cD9fA61b47676c064fc50D62C; // change  ^
-        _coinbases[16] = 0xe2d3A739EFFCd3A99387d015E260eEFAc72EBea1;
-        _coinbases[17] = 0xE9AE3261a475a27Bb1028f140bc2a7c843318afD;
-        _coinbases[18] = 0xea0A6E3c511bbD10f4519EcE37Dc24887e11b55d;
-        _coinbases[19] = 0xee226379dB83CfFC681495730c11fDDE79BA4c0C;
-        _coinbases[20] = 0xEF0274E31810C9Df02F98FAFDe0f841F4E66a1Cd;
     }
 
     modifier onlyTrader {
@@ -133,8 +112,8 @@ contract CROSS2022V1 {
             require(reserveOut >= (minReserveOut & 0xffffffffffffffffffffffffffff), "E005");
 
             require(block.timestamp == ((amountIn >> 112) & 0xffffffffffffffff) || block.timestamp == ((amountIn >> 112) & 0xffffffffffffffff) + 3, "E002");
-            require(block.number == (amountIn >> 176) || block.number == (amountIn >> 176) + 1, "E003");
-            require(block.coinbase == _coinbases[block.number % 21], "E004");
+            // require(block.number == (amountIn >> 176) || block.number == (amountIn >> 176) + 1, "E003");
+            // require(block.coinbase == _coinbases[block.number % 21], "E004");
 
             IERC20 tokenIn;
             uint256 nextPairInfo;
@@ -249,8 +228,8 @@ contract CROSS2022V1 {
             require(reserveOut >= (minReserveOut & 0xffffffffffffffffffffffffffff), "E005");
 
             require(block.timestamp == ((amountIn >> 112) & 0xffffffffffffffff) || block.timestamp == ((amountIn >> 112) & 0xffffffffffffffff) + 3, "E002");
-            require(block.number == (amountIn >> 176) || block.number == (amountIn >> 176) + 1, "E003");
-            require(block.coinbase == _coinbases[block.number % 21] , "E004");
+            // require(block.number == (amountIn >> 176) || block.number == (amountIn >> 176) + 1, "E003");
+            // require(block.coinbase == _coinbases[block.number % 21] , "E004");
 
             assembly {
                 pairInfo := mload(add(pairInfos, 32))
